dedf7313cc2b9ec8e01f7247db61e5cdd64c4d58
Steve Thompson <writetome51@gmail.com>
2018-10-20 18:07:50 -0600

    added tests and index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+
+var getCountup_getCountdown = require('./lib/getCountup_getCountdown');
+
+exports.getCountup = getCountup_getCountdown.getCountup;
+exports.getCountdown = getCountup_getCountdown.getCountdown;
---
+++ lib/tests.ts
@@ -0,0 +1,10 @@
+import { getCountdown, getCountup } from './getCountup_getCountdown';
+
+// Test 1
+let countup = getCountup(1, 10);
+console.log(countup);
+
+
+// Test 2
+let countdown = getCountdown(10, 1);
+console.log(countdown);
